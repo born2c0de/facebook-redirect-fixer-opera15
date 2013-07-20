15357e58755fa2136c9ebb050a3f51bdf68408b5
Sanchit Karve <write2sanchit@gmail.com>
2013-07-20 03:05:05 -0700

    Added feature to remove UntrustedLink redirects.

--- includes/fixFacebookRedirect.js
+++ includes/fixFacebookRedirect.js
@@ -8,7 +8,7 @@
     }
   }
   
-  function fixRedirect(event) {    
+var fixEvilRedirect = function(event) {    
     var node = event.target;
     if(!node.getAttribute('onmouseover') ||  !node.getAttribute('onclick')) {
       node = this;
@@ -16,28 +16,56 @@
     var mouseOverAttr = node.getAttribute('onmouseover');    
     node.removeAttribute('onclick');
     node.removeAttribute('onmouseover');    
-    node.removeEventListener('mouseover', fixRedirect, false);
+    node.removeEventListener('mouseover', fixEvilRedirect, false);
     var refPattern = /LinkshimAsyncLink.swap\(this\, \"(.*)\"\);/;
     mouseOverAttr = mouseOverAttr.replace(/&quot;/g,'"');    
     var realHref = unescape(refPattern.exec(mouseOverAttr)[1]);
     realHref = realHref.replace(/\\\//g, "/");
     node.href = realHref;
   };
+
+var fixRegularRedirect = function(event)
+{
+  var node = event.target;      
+    
+  // remove facebook's event handlers.
+  node.removeAttribute('onclick');
+  node.removeAttribute('onmousedown');
+  // disable this event handler
+  node.removeEventListener('mouseover',fixRegularRedirect,false);
+  
+  // Remove facebook redirection. 
+  var facebookRedirectString = "http://www.facebook.com/l.php?u=";  
+  var realHref = unescape(node.href.replace(facebookRedirectString,""));  
+  // set the href tag to the original URL as it should be.
+  node.href = realHref;
+
+};
   
   
   function huntForLinks() {     
     var items = document.getElementsByTagName('a');    
-    var mouseOverAttr;
+    var onMouseOverAttr;
     var onClickAttr;
-    var evilFunc = 'LinkshimAsyncLink.swap(this';
+    var onMouseDownAttr;
+
+    var evilFunc1 = 'LinkshimAsyncLink.swap(this';
+    var evilFunc2 = 'UntrustedLink.bootstrap(this';
 
     for (var i = items.length - 1; i >= 0; i--) {
-      mouseOverAttr = items[i].getAttribute('onmouseover');
+      onMouseOverAttr = items[i].getAttribute('onmouseover');
       onClickAttr = items[i].getAttribute('onclick');
+      onMouseDownAttr = items[i].getAttribute('onmousedown');
       
-      if (mouseOverAttr && onClickAttr) {        
-        if (mouseOverAttr.indexOf(evilFunc) !== -1 && onClickAttr.indexOf(evilFunc) !== -1) {
-          items[i].addEventListener('mouseover', fixRedirect, false);          
+      if (onMouseOverAttr && onClickAttr) {        
+        if (onMouseOverAttr.indexOf(evilFunc1) !== -1 && onClickAttr.indexOf(evilFunc1) !== -1) {
+          items[i].addEventListener('mouseover', fixEvilRedirect, false);          
+        }
+      }
+
+      if(onMouseDownAttr) {
+        if(onMouseDownAttr.indexOf(evilFunc2) !== -1) {          
+          items[i].addEventListener('mouseover',fixRegularRedirect,false);
         }
       }
     }
